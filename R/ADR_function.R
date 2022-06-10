ccd16344669251a386d397fa59b2663277a0edbc
JohanSalomonssonSV <61935963+JohanSalomonssonSV@users.noreply.github.com>
2022-06-10 21:21:57 +0200

    ADR days

--- R/ADR_function.R
+++ R/ADR_function.R
@@ -1,10 +1,11 @@
 #' ADR_function
 #' @param high high
 #' @param low low
+#' @param days days
 #' @importFrom roll roll_sum
 #' @export
 #' @return ADR
-ADR_function<-\( high="high", low="low"){
-  ADR<-(roll::roll_sum(high/low-1, width = 20)/20)*100
+ADR_function<-\( high="high", low="low", days=20){
+  ADR<-(roll::roll_sum(high/low-1, width = days)/days)*100
   return(ADR)
 }
