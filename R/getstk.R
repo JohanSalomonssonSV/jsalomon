789351f28e68b012319ecdb2215165bf78e603fb
JohanSalomonssonSV <61935963+JohanSalomonssonSV@users.noreply.github.com>
2022-11-25 10:01:57 +0100

    end_date

--- R/getstk.R
+++ R/getstk.R
@@ -1,13 +1,14 @@
 #' getstk
 #' @param ticker ticker
 #' @param start_date start_date
+#' @param end_date end_date
 #' @importFrom lubridate today 
 #' @importFrom tidyquant tq_get
 #' @export
 #' @return df
-getstk<-function(ticker, start_date="2015-01-01"){
+getstk<-function(ticker, start_date="2015-01-01", end_date=lubridate::today()){
   df <-ticker |>
-    tidyquant::tq_get( from = start_date, to = lubridate::today()#, periodicity = "weekly"
+    tidyquant::tq_get( from = start_date, to = end_date#, periodicity = "weekly"
     )
   
   return(df)
