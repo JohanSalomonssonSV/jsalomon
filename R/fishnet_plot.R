b642bf02a73c12d85ac0515423fdba80d27e5420
JohanSalomonssonSV <61935963+JohanSalomonssonSV@users.noreply.github.com>
2022-12-01 15:12:16 +0100

    volume fishnet

--- R/fishnet_plot.R
+++ R/fishnet_plot.R
@@ -4,7 +4,7 @@
 #' @param end_date end_date
 #' @param save_plot save_plot T/F
 #' @importFrom tidyquant geom_candlestick
-#' @importFrom ggplot2 ggplot geom_abline geom_tile geom_col geom_label labs theme_minimal geom_line scale_fill_identity geom_vline scale_color_identity geom_segment scale_alpha_identity scale_fill_manual
+#' @importFrom ggplot2 ggplot geom_tile geom_col geom_label labs theme_minimal geom_line scale_fill_identity geom_vline geom_hline scale_color_identity geom_segment scale_alpha_identity scale_fill_manual
 #' @importFrom dplyr mutate select filter  
 #' @importFrom scales date_format
 #' @importFrom bdscale scale_x_bd
@@ -20,7 +20,8 @@
 fishnet_plot<-function(symbol="^GSPC", start_date="2021-01-01", end_date=lubridate::today(), save_plot=TRUE){
   
   df<-jsalomon::getstk(symbol, start_date = lubridate::ymd(start_date)-320, end_date = end_date)   
-  df<- df |> dplyr::filter(!is.na(close))  
+  df<- df |> dplyr::filter(!is.na(close)) |> 
+    mutate(sma_vol_50=roll::roll_mean(volume, width = 50))
   fishnet<-function(close=close){
     
     t<-purrr::map_dfc(c(seq(10, 200,3),200), function(x){ 
@@ -37,8 +38,10 @@ fishnet_plot<-function(symbol="^GSPC", start_date="2021-01-01", end_date=lubrida
       tidyr::pivot_longer(cols = 2:66, values_to = "close") |> 
       dplyr::filter(date>=start_date)
     
-    df |> 
-      dplyr::filter(date>=start_date) |> 
+    plot_data<-df |> 
+      dplyr::filter(date>=start_date)
+    
+    pp<-plot_data |> 
       ggplot2::ggplot(aes(date,close))+
       # ggplot2::geom_text(label=str_remove(symbol,".ST"), x=-Inf, y=Inf
       #                    
@@ -52,7 +55,7 @@ fishnet_plot<-function(symbol="^GSPC", start_date="2021-01-01", end_date=lubrida
                                        #color="grey20",
                                        #size=15,
                                        #alpha=0.7, 
-                                       gp=grid::gpar(fontsize=140, col="grey20")
+                                       gp=grid::gpar(fontsize=180, col="grey20")
                                        ), 
                         xmin = -Inf, 
                         xmax = Inf,
@@ -75,22 +78,77 @@ fishnet_plot<-function(symbol="^GSPC", start_date="2021-01-01", end_date=lubrida
                                   colour_down = "#BC5D2E" ,
                                   fill_up   = "#7C9BDD"  ,
                                   fill_down = "#F34213" )+
+      ggplot2::geom_hline(aes(yintercept=ifelse(date==max(date), close,NA)), color="cyan",lty=5,size=0.1)+
+      ggplot2::geom_label(data=filter(plot_data,date==max(date)),
+                          aes(label=round(close,2),
+                              #y=close, 
+                              x=min(plot_data$date) 
+                          ),
+                          #hjust=-.1,
+                          fill="black",
+                          size=2.5,
+                          color="cyan"
+      )+
       #scale_color_identity()+
-      #scale_y_continuous(labels = scales::dollar)+
-      bdscale::scale_x_bd(business.dates=df$date, 
+      scale_y_continuous(#labels = scales::dollar
+        sec.axis = sec_axis( trans=~.)
+                         )+
+      bdscale::scale_x_bd(business.dates=plot_data$date, 
                           max.major.breaks=10, 
                           labels=scales::date_format("%b-'%y"))+
       ggplot2::labs(#title=symbol,
-        x="", y="",
-        caption = paste0("Data: Yahoo Finance, ", lubridate::today(), "\n@stops2breakeven")
+        x="", y=""
       )+
       jsalomon::theme_bors()+
       ggplot2::theme(legend.position = "none",
                      panel.grid.major.x = element_blank(),
                      panel.grid.minor.x = element_blank(),
                      axis.title.x = element_blank(),
+                     axis.text.x = element_blank(),
                      text = element_text(family = "sans")
       )
+    
+    
+    v<-
+      plot_data |> #dplyr::filter(date>=start_date) |>
+      ggplot2::ggplot(aes(x = date, y = volume))+
+      # ggplot2::geom_col(aes(fill=ifelse(close>dplyr::lag(close), "green", "red")))+
+      ggplot2::geom_col(aes(fill=ifelse(close>dplyr::lag(close), "#648DE5", "#BC5D2E"),
+                            alpha=ifelse(volume>sma_vol_50, 0.9,0.3)
+      ))+
+      ggplot2::geom_vline(aes(xintercept=date[which.min(sma_vol_50)]), color="grey80",lty=1, size=0.2)+
+      ggplot2::geom_line(aes(y=sma_vol_50,group=1, color=ifelse(sma_vol_50>dplyr::lag(sma_vol_50), "cyan", "orange")))+
+      ggplot2::scale_alpha_identity()+
+      ggplot2::scale_fill_identity()+
+      ggplot2::scale_color_identity()+
+      #ggplot2::scale_shape_identity()+
+      ggplot2::geom_hline(aes(yintercept=ifelse(date==max(date), volume,NA)), color="cyan",lty=5,size=0.1)+
+      bdscale::scale_x_bd(business.dates=df$date, max.major.breaks=10, labels=scales::date_format("%b-'%y"))+
+      labs(x="",y=NULL, 
+           caption = paste0("Data: Yahoo Finance, ", lubridate::today(), "\n@stops2breakeven"))+
+      jsalomon::theme_bors()+
+      theme(legend.position = "none",
+            axis.text.y = element_blank(),
+            axis.title.x =element_blank()
+      )
+    
+    
+    layout <- "
+AAAAAA
+AAAAAA 
+AAAAAA
+AAAAAA
+AAAAAA
+AAAAAA
+AAAAAA
+AAAAAA
+BBBBBB
+BBBBBB
+"
+    p<-patchwork::wrap_plots(pp,v, design = layout) 
+    p  
+    
+    
   }
   p<-plot_fishnet(start_date = start_date)  
   
